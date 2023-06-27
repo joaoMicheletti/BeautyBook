c504205305520c7883b9b794ca505df07867f7be
João Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2023-06-26 22:35:20 -0300

    agendados
    Definido o layout da div responsável por
    mostrar os agendamentos da data selecionada

--- frontend/src/pages/painel/painel.js
+++ frontend/src/pages/painel/painel.js
@@ -43,10 +43,88 @@ export default function Painel(){
                          <button id="BtnAgenda" type="sybmit">Buscar</button>
                     </div>
                 </div>
-
                 <br/>
                 <hr/>
                 <br/>
+                <div id="Agendados">
+                    <ul>
+                        <li>
+                            <p className="UnderLine" >20/06/1998</p>
+                            <p>Horário : 15:30</p>
+                            <p>Cliente :  Bianca de Andrade Cobra Micheletti</p>
+                            <p>WhatsApp Cliente : </p> 
+                            <a target="_blank"
+                            rel='noreferrer'  
+                            href='https://api.whatsapp.com/send?phone=5511932223533&text=Ol%C3%A1,%20Passando%20para%20lembrar-lhe%20que%20hoje%20voc%C3%AA%20tem%20um%20Hor%C3%A1rio%20marcado%20conosco.%20Posso%20Confirmar?' 
+                            className="LKWhatsapp">11932223533</a>
+                            <p>Funcionario :  Joao Victor de Andrade Micheletti</p>
+                            <p>Serviços :  Escova chapinha e tudo que há de bom em seguifda tem um churrasco</p>
+                            <p className="UnderLine">1.500,00R$</p><br/>
+                            <div className="DivStatus">
+                                <button className="BtnStatus" >Cancelado</button>
+                                <button className="BtnStatus" >finalizado</button>
+                            </div>
+                        </li>
+                    </ul>   
+                    <ul>
+                        <li>
+                            <p className="UnderLine" >20/06/1998</p>
+                            <p>Horário : 15:30</p>
+                            <p>Cliente :  Bianca de Andrade Cobra Micheletti</p>
+                            <p>WhatsApp Cliente : </p> 
+                            <a target="_blank"
+                            rel='noreferrer'  
+                            href='https://api.whatsapp.com/send?phone=5511932223533&text=Ol%C3%A1,%20Passando%20para%20lembrar-lhe%20que%20hoje%20voc%C3%AA%20tem%20um%20Hor%C3%A1rio%20marcado%20conosco.%20Posso%20Confirmar?' 
+                            className="LKWhatsapp">11932223533</a>
+                            <p>Funcionario :  Joao Victor de Andrade Micheletti</p>
+                            <p>Serviços :  Escova chapinha e tudo que há de bom em seguifda tem um churrasco</p>
+                            <p className="UnderLine">1.500,00R$</p><br/>
+                            <div className="DivStatus">
+                                <button className="BtnStatus" >Cancelado</button>
+                                <button className="BtnStatus" >finalizado</button>
+                            </div>
+                        </li>
+                    </ul>   
+                    <ul>
+                        <li>
+                            <p className="UnderLine" >20/06/1998</p>
+                            <p>Horário : 15:30</p>
+                            <p>Cliente :  Bianca de Andrade Cobra Micheletti</p>
+                            <p>WhatsApp Cliente : </p> 
+                            <a target="_blank"
+                            rel='noreferrer'  
+                            href='https://api.whatsapp.com/send?phone=5511932223533&text=Ol%C3%A1,%20Passando%20para%20lembrar-lhe%20que%20hoje%20voc%C3%AA%20tem%20um%20Hor%C3%A1rio%20marcado%20conosco.%20Posso%20Confirmar?' 
+                            className="LKWhatsapp">11932223533</a>
+                            <p>Funcionario :  Joao Victor de Andrade Micheletti</p>
+                            <p>Serviços :  Escova chapinha e tudo que há de bom em seguifda tem um churrasco</p>
+                            <p className="UnderLine">1.500,00R$</p><br/>
+                            <div className="DivStatus">
+                                <button className="BtnStatus" >Cancelado</button>
+                                <button className="BtnStatus" >finalizado</button>
+                            </div>
+                        </li>
+                    </ul>   
+                    <ul>
+                        <li>
+                            <p className="UnderLine" >20/06/1998</p>
+                            <p>Horário : 15:30</p>
+                            <p>Cliente :  Bianca de Andrade Cobra Micheletti</p>
+                            <p>WhatsApp Cliente : </p> 
+                            <a target="_blank"
+                            rel='noreferrer'  
+                            href='https://api.whatsapp.com/send?phone=5511932223533&text=Ol%C3%A1,%20Passando%20para%20lembrar-lhe%20que%20hoje%20voc%C3%AA%20tem%20um%20Hor%C3%A1rio%20marcado%20conosco.%20Posso%20Confirmar?' 
+                            className="LKWhatsapp">11932223533</a>
+                            <p>Funcionario :  Joao Victor de Andrade Micheletti</p>
+                            <p>Serviços :  Escova chapinha e tudo que há de bom em seguifda tem um churrasco</p>
+                            <p className="UnderLine">11.500,00R$00</p> <br/>
+                            <div className="DivStatus">
+                                <button className="BtnStatus" >Cancelado</button>
+                                <button className="BtnStatus"> Finalizado</button>
+                            </div>
+                        </li>
+                    </ul>   
+
+                </div>
             </section>
         </div>
     );
