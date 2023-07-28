eaa50f8efed0f8e4f1ff0d81d4a9e912731c339f
João Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2023-07-27 22:42:03 -0300

    lista de funcionários do salão
    foi criado uma função para listar os funcionários de um salão para que o usuário possa escolher com quem agendar um horário.

--- frontend/src/pages/agendamentofuncionario/agendametofuncionario.js
+++ frontend/src/pages/agendamentofuncionario/agendametofuncionario.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
+import Api from "../../services/api";
 import './style_ag_funcionario.css';
 import Logo from '../assets/Logo.png';
 
 export default function AgendamentoFuncionario(){
+    const History = useNavigate();
+    const [ListaFuncionarios, setListaFuncionarios] = useState([]);
+    useEffect(() =>{
+        var cpf_salao = localStorage.getItem('cpf_salao');
+        const Data = {
+            cpf_salao
+        };
+        console.log(Data);
+        Api.post('/funcionarios', Data).then((Response) =>{
+            console.log(Response)
+            setListaFuncionarios(Response.data);
+            if(Response.data.length === 0){
+                alert('funcionários não encontrados');
+            };
+        }).catch(() =>{
+            alert('Erro ao buscar por funcionários.');
+        });
+        
+    }, []);
     return(
         <div id="AgendamentoFuncionarioConteiner">
             <header id="HeaderAgFuncionario">
@@ -13,31 +34,24 @@ export default function AgendamentoFuncionario(){
                 Agende com um de nossos Profissionais
             </h1>
             <div id="ListFuncionarios">
-                <ul>
-                    <li>
-                        <p id="PListFuncionarios">
-                            Agendar Com : João Micheletti
-                        </p>
-                        <button className="BtnListFuncionarios">Selecionar</button>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <p id="PListFuncionarios">
-                            Agendar Com : João Micheletti
-                        </p>
-                        <button className="BtnListFuncionarios">Selecionar</button>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <p id="PListFuncionarios">
-                            Agendar Com : João Micheletti
-                        </p>
-                        <button className="BtnListFuncionarios">Selecionar</button>
-                    </li>
-                </ul>
-
+                {ListaFuncionarios.map((iten, key) =>{
+                    const AgendaFuncionario = () => {
+                        localStorage.setItem('cpf_funcionario', iten.cpf_funcionario);
+                        History('/agendamento');
+                    };
+                    return(
+                        <ul key={iten.id}>
+                            <li>
+                                <p id="PListFuncionarios">
+                                    Agendar Com : {iten.nome_completo}
+                                </p>
+                                <button className="BtnListFuncionarios"
+                                onClick={AgendaFuncionario}>Selecionar</button>
+                            </li>
+                        </ul>
+                    );
+                })}
+                
             </div>
         </div>
     );
