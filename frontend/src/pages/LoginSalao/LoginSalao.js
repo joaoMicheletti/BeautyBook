36c8769aa4e3dbec2f2f38ac3bfe2989c10ee181
João Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2023-08-05 03:04:16 -0300

    login
    finalizado o login das entidades.

--- frontend/src/pages/LoginSalao/LoginSalao.js
+++ frontend/src/pages/LoginSalao/LoginSalao.js
@@ -1,31 +1,61 @@
 import React, {useState} from "react";
 import Logo from '../assets/Logo.png';
 import './style.css';
+import Api from '../../services/api';
+import {useNavigate} from 'react-router-dom';
 
 export default function LoginSalao(){
-    const [Cpf,  setCpf] = useState('');
-    const [Senha, setSenha] = useState('');
+    const Hystory = useNavigate();
+    const [cpf_salao,  setCpf] = useState('');
+    const [senha, setSenha] = useState('');
 
     
     const Logar = async (e) => {
         e.preventDefault();
         const Data = {
-            Cpf,
-            Senha
+            cpf_salao,
+            senha
         };
-        console.log(Data);
-        if(Cpf.length === 0){
+
+        if(cpf_salao.length === 0){
             alert('Preencha o campo Cpf');
-        }else if(Cpf.length > 11) {
+        }else if(cpf_salao.length > 11) {
             alert('Cpf ou Senha invalido');
-        } else if(Cpf.length < 11){
+        } else if(cpf_salao.length < 11){
             alert('Cpf ou Senha invalido');
-        } else if(Senha.length === 0){
+        } else if(senha.length === 0){
             alert('Preencha o campo Senha');
         } else {
-            alert('enviar para a api');
         };
-    }
+        await Api.post('/loginsalao', Data).then((Response) => {
+            console.log(Response.data);
+            if(Response.data === 'não encontramos nenhum salão ou funcionário com esses dados!'){
+                alert(Response.data);
+            } else if(Response.data === 'erro no login'){
+                alert("Usuário ou senha invalidos.");
+            } else if(Response.data === 'Acesso Negado, problemas com à assinatura do plano.'){
+                alert(Response.data);                
+            } else{
+                //logado com sucessu;
+                //salvar no localstorage o cof_salão;
+                //salvando a resposta na variavel data;
+                var data = Response.data;
+                // si data.cpf_funcionario === undefined; logado como salão 
+                if(data.cpf_funcionario === undefined){
+                    localStorage.setItem('cpf_salao', data.cpf_salao);
+                    //encaminha para o painel;
+                    alert("Logado com sucesso, Bom Trabalho.");
+                    Hystory('/painel');                    
+                } else {
+                    //logado como funcionário;
+                    //encaminar para painel do funcionário;
+                    localStorage.setItem('cpf_funcionario', data.cpf_funcionario);
+                };
+            }
+        }).catch((Erro) => {
+            alert('Erro interno.');
+        });
+    };
     return(
         <div id="LoginSalao">
             <header id="HeaderLoginSalao">
