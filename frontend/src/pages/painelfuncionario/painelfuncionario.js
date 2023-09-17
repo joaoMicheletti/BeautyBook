0f4ead96592fe375cdc29bac5eb9fcd211f5a0b8
Micheletti <jv604014@gmail.com>
2023-09-16 23:06:25 -0300

    criado o painel do funcionario

---
+++ frontend/src/pages/painelfuncionario/painelfuncionario.js
@@ -0,0 +1,93 @@
+import React, {useState, useEffect} from 'react';
+//import { useNavigate } from 'react-router-dom';
+import Api from '../../services/api';
+import './painelfuncionario.css';
+export default function PainelFuncionario(){
+    //const History = useNavigate();
+    const DataAtual = new Date();// oibjeto data atual;
+    //o servidor espera receber semaradamente o diam, mes e ano,
+    var dia = parseInt(DataAtual.getDate(), 10);
+    var mes = parseInt(DataAtual.getMonth(), 10) + 1;
+    var ano = parseInt(DataAtual.getFullYear(), 10); 
+    //pegando do storage o cpf do salão;
+    var cpf_funcionario = localStorage.getItem('cpf_fincionario');
+    const [Agendados, setAgendados] = useState([]);
+    const Data = {
+        dia, mes, ano, cpf_funcionario
+    };
+    
+    useEffect( () =>{
+        Api.post('/horariospreenchidos', Data).then((Response) =>{
+            setAgendados(Response.data);
+            
+        }).catch((erro) =>{
+            alert('Erro interno.');
+        });
+    }, []); //não passar dependências retorna um alerta ! porem ao passar o Data como dependência ele cai num loop infinito de requisições.
+    
+    return(
+        <div>
+            <header className='cabeçalho'>
+                <h1 id="TitleFuncionario" >Olá funcionário</h1>
+            </header>
+            <br/>
+            <h2>Agenda do Dia.</h2>
+            <div className='ContainerAgenda'>
+            {Agendados.map((iten, key) =>{
+                //funções de cancelar e finalizar;
+                //cancelar;
+                const Cancelar = async (e) =>{
+                    var id = iten.id;
+                    var Data = {
+                        id
+                    };                            
+                    await Api.put('/cancelarservico', Data).then((Response) =>{
+                        alert(Response.data);
+                    }).catch(() =>{
+                        alert('Erro interno.')
+                    });
+                    };
+                    //finalizar;
+                    const Finalizar = async (e) =>{
+                        var id = iten.id;
+                        var Data = {
+                            id
+                        };                            
+                        await Api.put('/finalizarservico', Data).then((Response) =>{
+                            alert(Response.data);
+                        }).catch(() =>{
+                            alert('Erro interno.')
+                        });
+                    };
+                    return(
+                        <ul key={iten.id}>
+                            <li>
+                            <p className="UnderLine" >{iten.dia}/{iten.mes}/{iten.ano}</p>
+                            <br/>
+                            <p>Horário : {iten.hora}  AS {iten.hora_termino}</p>
+                            <p>Cliente :  {iten.nome_cliente}</p>
+                            <p>WhatsApp Cliente : </p> 
+                            <a target="_blank"
+                            rel='noreferrer'  
+                            href='https://api.whatsapp.com/send?phone=5511932223533&text=Ol%C3%A1,%20Passando%20para%20lembrar-lhe%20que%20hoje%20voc%C3%AA%20tem%20um%20Hor%C3%A1rio%20marcado%20conosco.%20Posso%20Confirmar?' 
+                            className="LKWhatsapp">{iten.contato_cliente}</a>
+                            <br/>
+                            <br/>
+                            <p> Funcionário : {iten.nome_completo}</p>
+                            <p>Serviços :  {iten.servico}</p>
+                            <p>Observação : {iten.obs}
+                            </p>
+                            <br/>
+                            <p className="UnderLine">Valro Serviço : {iten.preco} _R$</p><br/>
+                            <div className="DivStatus">
+                                <button className="BtnStatus" onClick={Cancelar}>Cancelar</button>
+                                <button className="BtnStatus" onClick={Finalizar}>finalizado</button>
+                            </div>
+                            </li>
+                        </ul>
+                        );                      
+                    })}
+            </div>
+        </div>
+    );
+};
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import Init from './pages/init/init';
 import RegistroSalao from './pages/RegistroSalao/RegistroSalao';
 import LoginSalao from './pages/LoginSalao/LoginSalao';
 import Paninel from './pages/painel/painel';
+import PainelFuncionario from './pages/painelfuncionario/painelfuncionario';
 import Servicos from './pages/servicos/servicos';
 import Funcionarios from './pages/funcionarios/funcionarios';
 import HorarioFuncionamento from './pages/horariofuncionamento/horariofuncionamento';
@@ -30,6 +31,7 @@ export default function Rotas(){
                 <Route path='/registrosalao' element={<RegistroSalao/>}/>
                 <Route path='/loginsalao' element={<LoginSalao/>}/>
                 <Route path='/painel' element={<Paninel/>}/>
+                <Route path='/painelfuncionario' element={<PainelFuncionario/>}/>
                 <Route path='/servicos' element={<Servicos/>}/>
                 <Route path='/funcionarios' element={<Funcionarios/>}/>
                 <Route path='/funcionamento' element={<HorarioFuncionamento/>}/>
