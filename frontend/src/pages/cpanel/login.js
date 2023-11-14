8b27834b2e8e252e2c2a4c8ccda3d9d2e7f0f2ea
Micheletti <jv604014@gmail.com>
2023-11-14 12:36:12 -0300

    finalizada a etapa de login

--- frontend/src/pages/cpanel/login.js
+++ frontend/src/pages/cpanel/login.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import Logo from '../assets/Logo.png';
 import './login.css';
+import Api from '../../services/api';
 export default function LoginCpanel(){
+    async function Entrar(){
+        const Data = {
+            user: document.querySelector('.user').value,
+            pass: document.querySelector('.pass').value,
+        };
+        if(Data.user === ''){
+            alert("Preencha o campo USER");
+        } else if (Data.pass === ''){
+            alert("Preencha o campo pass");
+        } else {
+            Api.post('/cpanellogin', Data).then((Response) => {
+                if (Response.data === 'Erro no login'){
+                    alert('Erro no login');
+                } else{
+                    localStorage.setItem('user', Response.data.user);
+                    localStorage.setItem('pass', Response.data.pass);
+                    //redirect;
+                }
+            }).catch((Erro) => {
+                alert('Erro na conexão com o servidor');
+            });
+        };
+    };
     return(
         <div id='LoginCpanel'>
             <header id='HeaderCpanel'>
-                <img src={Logo}  />
+                <img src={Logo} alt='logos' />
             </header>
-            <section id='ConteudoCpanelLogin'>
+            <div id='ConteudoCpanelLogin'>
                 <div id='FormLoginCpanel'>
                     <p>Login</p>
                     <label>User:  
-                        <input id='InoCpanel' type='text' placeholder=' User'/>
+                        <input id='InoCpanel' className='user' type='text' placeholder=' User'/>
                     </label>
                     <br/>
                     <label>Pass:  
-                        <input id='InoCpanel' type='password' placeholder=' User'/>
+                        <input id='InoCpanel' className='pass' type='password' placeholder=' User'/>
                     </label>
                     <br/>
-                    <button>Entrar</button>
+                    <input onClick={Entrar} type='submit' value="Entrar"/>
                 </div>
-            </section>
+            </div>
         </div>
     );
 };
